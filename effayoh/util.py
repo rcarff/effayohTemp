223db3d2e2e2e6e523d80d17edda1db82aacc6eb
Omid <omid.bakhshandeh@live.com>
2017-09-14 19:51:51 -0400

    changing dirs

--- effayoh/util.py
+++ effayoh/util.py
@@ -1,8 +1,9 @@
 import os
 import csv
 
+TRIPS_BASE = os.environ['TRIPS_BASE']
 EFFAYOH_DIR, _ = os.path.split(__file__)
 RESOURCES_DIR = os.path.join(EFFAYOH_DIR, "resources")
-FAOSTAT_DIR = os.path.join(RESOURCES_DIR, "faostat")
+FAOSTAT_DIR = os.path.join(TRIPS_BASE, "faostat")
 USDA_DIR = os.path.join(RESOURCES_DIR, "usda")
 PSD_DIR = os.path.join(USDA_DIR, "psd")
